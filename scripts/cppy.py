fixed the way covariance and correlation matrices are calculated so it's faster and doesn't calculate redundant stuff. TO DO: think of a better way to calculate two point distance dependent autocorrelation, while averaging lag... tricky

scripts/cppy.py
@@ -59,15 +59,16 @@ class Protein:
     '''
 
     def __init__(self, prot, strd=0, mask="@CA", calc_all=False,
-                 load_saved=False, save_new=False, temperature=300):
+                 load_saved=False, save_new=False, temperature=300, inds=None):
         self.prot = prot
         self.strd = strd
         self.mask = mask
+        self.inds = inds
         self.temperature = temperature
         # Protein code as it appears in RCSB Protein Data Bank
         self.PDB = prot[:-3].upper() if '_sh' in prot else prot.upper()
         self.data_path = 'temp{}K/data_{}'.format(self.temperature, self.PDB)
-        self.absol = '..' #path to raw_data folder
+        self.absol = '/mnt/disk2/lucia/criticality/globular'
 
         print('\nProcessing {}'.format(self.PDB))
         self.fetch_data(load_saved, calc_all, save_new)
@@ -93,9 +94,16 @@ class Protein:
         if save_new:
             self.save_data()
 
+    def info(self):
+        print('\nPROTEIN INFORMATION')
+        print('PDB code: {}'.format(self.PDB))
+        print('residues: {}'.format(self.atoms))
+        print('frames: {}'.format(self.frames))
+        print('temperature: {}'.format(self.temperature))
+
     def load_data(self):
         with open(os.path.join(self.absol, 'processed_data', self.data_path), 'rb') as f:
-            # there's some issue with encoding and pickle in python 3
+            # there's some issue with pickle and encoding in python 3
             loadedData = pkl.load(f, encoding='latin1')
 
             self.ppal = loadedData['ppal']
@@ -117,11 +125,7 @@ class Protein:
             self.avgd_corr = pd.DataFrame(loadedData['avgd_correlation'])
 
             print('Loaded data_{}'.format(self.PDB))
-            print('\nPROTEIN INFORMATION')
-            print('PDB code: {}'.format(self.PDB))
-            print('residues: {}'.format(self.atoms))
-            print('frames: {}'.format(self.frames))
-            print('temperature: {}'.format(self.temperature))
+            self.info() 
 
     def prepare(self):
         '''Load trajectory, print information and
@@ -130,12 +134,12 @@ class Protein:
         self.atoms = self.traj.n_atoms
         self.frames = self.traj.n_frames
 
-        print('\nPROTEIN INFORMATION')
-        print('PDB code: {}'.format(self.PDB))
-        print('residues: {}'.format(self.atoms))
-        print('frames: {}'.format(self.frames))
-        print('temperature: {}'.format(self.temperature))
+        self.info() 
+        self.clean_traj()
+        # Initialize arrays
+        self.initialize_arrays()
 
+    def clean_traj(self):
         # Align molecule to principal axes
         pt.principal_axes(self.traj, dorotation=True)
         # Center molecule at the origin
@@ -146,8 +150,6 @@ class Protein:
         self.avg = pt.mean_structure(self.traj)
         # Perform rms fit to average structure
         pt.rmsd(self.traj, ref=self.avg)
-        # Initialize arrays
-        self.initialize_arrays()
 
     def initialize_arrays(self):
         # fluctuations
@@ -155,11 +157,11 @@ class Protein:
         # magnitude of fluctuations
         self.fluct_norms = []
         # covariance coeficients matrix
-        self.cov = np.zeros((self.traj.n_atoms, self.traj.n_atoms))
+        self.cov = np.zeros((self.atoms, self.atoms))
         # correlation coefficients matrix
-        self.corr = np.zeros((self.traj.n_atoms, self.traj.n_atoms))
+        self.corr = np.eye(self.atoms)
         # distance between residues matrix
-        self.dist = np.zeros((self.traj.n_atoms, self.traj.n_atoms))
+        self.dist = np.zeros((self.atoms, self.atoms))
         # averaged correlations
         self.avgd_corr = pd.DataFrame()
         # correlation length
@@ -171,31 +173,9 @@ class Protein:
         # time averaged MSD
         self.TA_MSD = []
         # autocorrelation
-        self.autocorr = []
-
-    def calc_all(self):
-        # calculate fluctuations
-        self.calc_fluct()
-        # calculate covariances
-        self.calc_cov()
-        # calculate correlations
-        self.calc_corr()
-        # calculate principal axes (length)
-        self.calc_ppal_axes()
-        # calculate radius of gyrations
-        self.rg = np.mean(pt.radgyr(self.traj))
-        # computes average distance between residues (~3.8A = .38nm)
-        tem_dist = [self.dist[i][i+1] for i in range(self.traj.n_atoms - 1)]
-        self.avg_dist = np.mean(tem_dist)
-        # calculate shape factor fluctuations and mean value
-        self.shape_factor_fluct()
-        self.shape_factor = np.mean(self.SF_fluct)
-        # compute suceptibility
-        self.calc_suceptibility()
-        # compute TA-MSD
-        # self.calc_TA_MSD() # not yet
-        # compute autocorrelation in terms of the lag
-        # self.calc_autocorr() # not yet
+        self.C_r = {}
+        #self.t_cov = np.zeros((self.frames, self.atoms, self.atoms))
+        #self.t_dist = np.zeros((self.frames, self.atoms, self.atoms))
 
     def save_data(self):
         '''Save all data to binary file.'''
@@ -230,28 +210,34 @@ class Protein:
         traj = os.path.join(files, 'nowat.{}.nc'.format(self.prot))
         # topology path
         top = os.path.join(files, 'nowat.{}.prmtop'.format(self.prot))
-        # load trajectory file
-        self.traj = pt.load(traj, top, mask=self.mask, stride=self.strd)
+        # load trajectory file, if both stride and frame_indices are
+        # given the latter is ignored!
+        if self.inds is not None:
+            self.traj = pt.load(traj, top, frame_indices=self.inds, mask=self.mask)
+        else:
+            self.traj = pt.load(traj, top, stride=self.strd, mask=self.mask)
 
-    def avg_corr(self):
-        '''Performs rolling average of correlation and calculated
-        correlation length from the resulting curve.'''
-        # flatten distance and correlation arrays, create dataframe
-        flat_corr = np.ndarray.flatten(np.array(self.corr))
-        flat_dist = np.ndarray.flatten(np.array(self.dist))
-        corr_dict = {'distance': flat_dist, 'correlation': flat_corr}
-        self.avgd_corr = pd.DataFrame(corr_dict)
-
-        # clean data and perform rolling average
-        self.avgd_corr.sort_values(by=['distance'], inplace=True)
-        self.avgd_corr = self.avgd_corr[self.avgd_corr.distance != 0]
-        self.avgd_corr = self.avgd_corr.rolling(self.atoms).mean()
-        self.avgd_corr.dropna(axis=0, inplace=True)
-
-        # correlation length defined as the first negative or zero correlation value
-        cl_idx = (self.avgd_corr.correlation <= 0).idxmax()
-        self.corr_len = self.avgd_corr.distance[cl_idx]
-        print(u'\u2713 averaged correlation')
+    def calc_all(self):
+        # calculate fluctuations
+        self.calc_fluct()
+        # calculate covariances and correlations
+        self.calc_cov()
+        # calculate principal axes (length)
+        self.calc_ppal_axes()
+        # calculate radius of gyrations
+        self.rg = np.mean(pt.radgyr(self.traj))
+        # computes average distance between residues (~3.8A = .38nm)
+        temp_dist = [self.dist[i][i+1] for i in range(self.traj.n_atoms - 1)]
+        self.avg_dist = np.mean(temp_dist)
+        # calculate shape factor fluctuations and mean value
+        self.shape_factor_fluct()
+        self.shape_factor = np.mean(self.SF_fluct)
+        # compute suceptibility
+        self.calc_suceptibility()
+        # compute TA-MSD
+        # self.calc_TA_MSD() # not yet
+        # compute autocorrelation in terms of the lag
+        #self.calc_autocov() 
 
     def calc_fluct(self):
         '''Calculate atom/residue fluctuation from the average position.'''
@@ -261,25 +247,30 @@ class Protein:
         print(u'\u2713 calculated fluctuations')
 
     def calc_cov(self):
-        '''Calculates covariance matrix from fluctuations.'''
+        '''Calculates covariance and correlation from fluctuations, and distance matrix.'''
         # Compute covariance coefficient matrix
+
+        # diagonal
         for i in range(self.atoms):
-            for j in range(self.atoms):
+            for frm in self.fluct:
+                self.cov[i][i] += np.sum([frm[i][q] * frm[i][q] for q in range(3)])
+            self.cov[i][i] /= self.frames
+        # outside diagonal, taking advantage of symmetry
+        for i in range(self.atoms):
+            for j in range(i+1, self.atoms):
                 for frm in self.fluct:
                     self.cov[i][j] += np.sum([frm[i][q] * frm[j][q] for q in range(3)])
                 self.cov[i][j] /= self.frames
-        print(u'\u2713 calculated covariance')
-
-    def calc_corr(self):
-        '''Calculates correlation and distance matrices.'''
-        # Compute correlation coefficient matrix
-        # Calculate mutual distances between residues
-        for i in range(self.atoms):
-            for j in range(self.atoms):
+                self.cov[j][i] = self.cov[i][j]
+                # correlation and distance
                 self.corr[i][j] = self.cov[i][j] / np.sqrt(self.cov[i][i] * self.cov[j][j])
+                self.corr[j][i] = self.corr[i][j]
+
                 self.dist[i][j] = pt.distance(
                     self.traj, ':{} :{}'.format(i+1, j+1), frame_indices=[0])[0]
-        print(u'\u2713 calculated correlations')
+                self.dist[j][i] = self.dist[i][j]
+
+        print(u'\u2713 calculated covariance and correlation')
 
     def calc_ppal_axes(self):
         '''Calculate average of principal axes length.'''
@@ -314,22 +305,64 @@ class Protein:
             self.SF_fluct.append(S)
         print(u'\u2713 calculated shape factor')
 
+    def avg_corr(self):
+        '''Performs rolling average of correlation and calculated
+        correlation length from the resulting curve.'''
+        # flatten distance and correlation arrays, create dataframe
+        flat_corr = np.ndarray.flatten(np.array(self.corr))
+        flat_dist = np.ndarray.flatten(np.array(self.dist))
+        corr_dict = {'distance': flat_dist, 'correlation': flat_corr}
+        self.avgd_corr = pd.DataFrame(corr_dict)
+
+        # clean data and perform rolling average
+        self.avgd_corr.sort_values(by=['distance'], inplace=True)
+        self.avgd_corr = self.avgd_corr[self.avgd_corr.distance != 0]
+        self.avgd_corr = self.avgd_corr.rolling(self.atoms).mean()
+        self.avgd_corr.dropna(axis=0, inplace=True)
+
+        # correlation length defined as the first negative or zero correlation value
+        cl_idx = (self.avgd_corr.correlation <= 0).idxmax()
+        self.corr_len = self.avgd_corr.distance[cl_idx]
+        print(u'\u2713 averaged correlation')
+
     def calc_suceptibility(self):
         '''Calculate suceptibility as the sum of correlation values
         up to the correlation length.'''
         self.avg_corr()
-        # x = self.avgd_corr['distance'][self.avgd_corr['distance'] <= self.corr_len]
         y = self.avgd_corr['correlation'][self.avgd_corr['distance'] <= self.corr_len]
-        self.suceptibility = (self.shape_factor / self.atoms) * sum(y)  # ?
+        self.suceptibility = (1 / self.atoms) * sum(y) 
         print(u'\u2713 calculated suceptibility')
 
-    def calc_autocorr(self):
-        '''Autocorrelation.'''
-        half = int(self.atoms / 2)
-        R = pd.Series(pt.distance(
-            self.traj, ':1-{} :{}-{}'.format(half, half, self.atoms)))
-        # [R.autocorr(lag=i) for i in range(self.frames-5)]
-        self.autocorr = pt.acorr(R)
+## this is awful
+#    def calc_autocov(self):
+#        # Compute autocorrelation
+#        # TO-DO: implement averaging of C_r (moving frm0)
+#        f_frm0 = self.fluct[0] # first frame fluct
+#        f_norm0 = np.mean([np.dot(f_frm0[i], f_frm0[i]) for i in range(self.atoms)])
+#        f_frm0 /= f_norm0
+#
+#        xyz0 = self.traj.xyz[0] # first coordinates frame
+#        dr = 5 # step in distance
+#
+#        for q in range(self.frames):
+#            xyz = self.traj.xyz[q]
+#            fluct = self.fluct[q]
+#            f_norm = np.mean([np.dot(fluct[i], fluct[i]) for i in range(self.atoms)])
+#            for i in range(self.atoms):
+#                for j in range(self.atoms):
+#                    f_frm = fluct[j]/f_norm # q-th fluct frame for j
+#                    d_frm = xyz[j] # q-th dist frame for j
+#                    t_dist = int(ln.norm(xyz0[i] - d_frm))
+#
+#                    r_val = t_dist - t_dist % dr 
+#                    AC_ij = self.C_r.get(r_val, pd.Series(np.zeros(self.frames)))
+#                    AC_ij[q] += np.sum(np.prod([f_frm0[i], f_frm], axis=0)) # falta contar la cantidad de veces que agrego esto, dividir por ese numero de veces
+#                    self.C_r[r_val] = AC_ij
+#                    #self.t_cov[q][i][j] = np.sum(np.prod([f_frm0[i], f_frm], axis=0))
+#                    #self.t_dist[q][i][j] = ln.norm(xyz0[i] - d_frm)
+#
+#        print(u'\u2713 calculated autocorrelation')
+
 
     def calc_TA_MSD(self):
         '''Time averaged mean squared deviation.'''